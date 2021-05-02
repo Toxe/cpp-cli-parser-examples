4de668e5a802386b421e6e05cad3bde1f44922ed
Toxe <toxe@toxe.net>
2021-05-02 11:45:21 +0200

    Add clipp.cpp

---
+++ src/clipp.cpp
@@ -0,0 +1,86 @@
+#include <filesystem>
+#include <string>
+#include <string_view>
+#include <tuple>
+
+#include <clipp.h>
+#include <fmt/core.h>
+#include <fmt/ostream.h>
+#include <spdlog/spdlog.h>
+
+enum class OutputFormat { Text, Raw, Pretty, Data };
+
+void show_usage_and_exit(const clipp::group& cli, const std::string& progname, const std::string_view& description, const std::string_view& example, const char* error_message = nullptr)
+{
+    fmt::print("{}", clipp::make_man_page(cli, progname)
+        .prepend_section("DESCRIPTION", fmt::format("    {}", description))
+        .append_section("EXAMPLE", fmt::format("    $ {} {}", progname, example)));
+
+    if (error_message)
+        fmt::print("\n{}\n", error_message);
+
+    std::exit(1);
+}
+
+auto eval_args(int argc, char* argv[])
+{
+    const auto progname{std::filesystem::path{argv[0]}.filename().string()};
+    const auto description = "CLI Parser Example (clipp)";
+    const auto example = "--text 10 10 out.txt";
+    bool show_help = false;
+    auto log_level = spdlog::level::warn;
+    int seed = -1;
+    int zoom = 1;
+    int width, height;
+    bool info = false;
+    OutputFormat output_format = OutputFormat::Text;
+    std::string filename;
+
+    auto cli = (
+        clipp::option("-h", "--help").set(show_help)                          % "show help",
+        clipp::option("-v", "--verbose").set(log_level, spdlog::level::debug) % "show verbose output",
+        (clipp::option("-t", "--text").set(output_format, OutputFormat::Text)     |
+         clipp::option("-p", "--pretty").set(output_format, OutputFormat::Pretty) |
+         clipp::option("-d", "--data").set(output_format, OutputFormat::Data)     |
+         clipp::option("-r", "--raw").set(output_format, OutputFormat::Raw))  % "output format (default: text)",
+        (clipp::option("-s", "--seed") & clipp::integer("seed", seed))        % "random seed (0 or bigger)",
+        (clipp::option("-z", "--zoom") & clipp::integer("zoom", zoom))        % "pixel zoom factor for .raw files (default: 1)",
+        clipp::option("-i", "--info").set(info)                               % "output additional info",
+        clipp::integer("width", width)                                        % "maze width",
+        clipp::integer("height", height)                                      % "maze height",
+        clipp::opt_value("filename", filename)                                % "output filename"
+    );
+
+    if (!clipp::parse(argc, argv, cli))
+        show_usage_and_exit(cli, progname, description, example);
+
+    spdlog::set_level(log_level);
+    spdlog::debug("command line option \"width\": {}", width);
+    spdlog::debug("command line option \"height\": {}", height);
+    spdlog::debug("command line option \"filename\": \"{}\"", filename);
+    spdlog::debug("command line option --text: {}", output_format == OutputFormat::Text);
+    spdlog::debug("command line option --pretty: {}", output_format == OutputFormat::Pretty);
+    spdlog::debug("command line option --data: {}", output_format == OutputFormat::Data);
+    spdlog::debug("command line option --raw: {}", output_format == OutputFormat::Raw);
+    spdlog::debug("command line option --seed: {}", seed);
+    spdlog::debug("command line option --zoom: {}", zoom);
+    spdlog::debug("command line option --info: {}", info);
+
+    if (show_help)
+        show_usage_and_exit(cli, progname, description, example);
+
+    if (width < 1 || height < 1)
+        show_usage_and_exit(cli, progname, description, example, "Error: Maze width and height need to be at least 1");
+
+    if (zoom < 1)
+        zoom = 1;
+
+    return std::make_tuple(seed, width, height, filename, output_format, zoom, info);
+}
+
+int main(int argc, char* argv[])
+{
+    auto [seed, width, height, filename, output_format, zoom, info] = eval_args(argc, argv);
+
+    spdlog::info("let's do something...");
+}
